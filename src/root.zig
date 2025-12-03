dc73d8dede43ce12d724a2c8ae24c5ae63fc039a
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-12-02 22:42:20 -0500

    Made some modifications to the root.zig file to better understand what I am doing in there

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,10 @@
 //! By convention, root.zig is the root source file when making a library.
-const std = @import("std");
+
+const std = @import("std"); // Provides access to the zig standard library
+const testing = std.testing;
 
 pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
@@ -14,10 +14,23 @@ pub fn bufferedPrint() !void {
     try stdout.flush(); // Don't forget to flush!
 }
 
-pub fn add(a: i32, b: i32) i32 {
+// export fn exposes the function to make it available to the library API if we were writing a library.
+
+export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
+// Creating a subtract function except taking in two 64 bit integers and return a 64 bit integer
+pub fn subtract(x: i64, y: i64) i64{
+    return x - y;
+}
+
+
 test "basic add functionality" {
     try std.testing.expect(add(3, 7) == 10);
+}
+
+// Creating a basic test for our subtract function
+test "basic subtract functionality"{
+    try std.testing.except(subtract(10,6) == 4);
 }
